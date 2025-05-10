0300518e0ca7689c1684874c6dbda5482ba0da17
Mateus Alves Conceição <101145627+mateusalvesc@users.noreply.github.com>
2025-05-10 02:58:50 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Mateus: Programar Transforma o Mundo.</h1>
+  return <h1>Mateus: Programar Transforma o Mundo.</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
